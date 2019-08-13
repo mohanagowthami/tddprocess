9357f5f6826a418a088228e6a6032090d458e3c3
ib_admin <mohanagowthami gajula>
2019-08-13 17:01:10 +0530

    green state of productstore products

--- src/components/stores/ProductStore/ProductStore.test.js
+++ src/components/stores/ProductStore/ProductStore.test.js
@@ -1,8 +1,7 @@
 import ProductStore from "./index";
-import { italic } from "ansi-colors";
 const productStore = new ProductStore();
 describe(" testing the productStore function", () => {
   it(" testing products array", () => {
-    expect(productStore.preDefinedProducts.length).toBe("4");
+    expect(productStore.preDefinedProducts.length).toBe(4);
   });
 });
--- src/components/stores/ProductStore/index.js
+++ src/components/stores/ProductStore/index.js
@@ -1,2 +1,14 @@
-class ProductStore {}
+import getproducts from "../../../fixtures/Products.json";
+import ProductModel from "../models/ProductModel/index";
+import { observable } from "mobx";
+class ProductStore {
+  preDefinedProducts = getproducts.products;
+  @observable products = [];
+
+  addProducts(id) {
+    let index = this.preDefinedProducts.findIndex(product => {
+      return product.id == id;
+    });
+  }
+}
 export default ProductStore;

79d6c7af2402f1049e72336a6f51247a009224a4
ib_admin <mohanagowthami gajula>
2019-08-14 09:38:53 +0530

    green state of name field

--- src/components/TddProcess/TddProcess.test.js
+++ src/components/TddProcess/TddProcess.test.js
@@ -1,15 +1,14 @@
 import TddProcess from "./index";
 import { render, fireEvent, cleanup } from "@testing-library/react";
 import React from "react";
-
 describe("should test the form", () => {
   const myMock = jest.fn();
   it("should test name fields", () => {
     const renderObject = render(<TddProcess />);
     const { getByTestId } = renderObject;
-    fireEvent.click(getByTestId("name"));
+    fireEvent.focus(getByTestId("name"));
     expect(getByTestId("validation-name").innerHTML).toBe(
-      "plese enter the name"
+      "please enter the name "
     );
   });
 });
--- src/components/TddProcess/index.js
+++ src/components/TddProcess/index.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
+@observer
+class TddProcess extends Component {
+  @observable input = "";
 
-export default class TddProcess extends Component {
+  @observable isFocus = false;
+  handlechnage = e => {
+    this.input = e.target.value;
+  };
+  handlefocus = () => {
+    this.isFocus = true;
+    console.log(this.isFocus);
+  };
+
   render() {
-    return <div />;
+    const error =
+      this.input == "" && this.isFocus == true ? "please enter the name" : "";
+    return (
+      <div>
+        <input
+          type="text"
+          data-testid="name"
+          onFocus={this.handlefocus}
+          onChange={this.handlechnage}
+        />
+        <div data-testid="validation-name">{error} </div>
+      </div>
+    );
   }
 }
+export default TddProcess;

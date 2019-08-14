4b88c3dfaaaed16b83143115825031fe8bd92589
ib_admin <mohanagowthami gajula>
2019-08-14 10:51:08 +0530

    red state of phone number field

--- src/components/TddProcess/TddProcess.test.js
+++ src/components/TddProcess/TddProcess.test.js
@@ -3,9 +3,9 @@ import { render, fireEvent, cleanup } from "@testing-library/react";
 import React from "react";
 describe("should test the form", () => {
   const myMock = jest.fn();
+  const renderObject = render(<TddProcess />);
+  const { getByTestId } = renderObject;
   it("should test name fields", () => {
-    const renderObject = render(<TddProcess />);
-    const { getByTestId } = renderObject;
     fireEvent.focus(getByTestId("name"));
     expect(getByTestId("validation-name").innerHTML).toBe(" ");
     fireEvent.blur(getByTestId("name"));
@@ -14,5 +14,22 @@ describe("should test the form", () => {
     );
     fireEvent.change(getByTestId("name"), { target: { value: "12" } });
     expect(getByTestId("validation-name").innerHTML).toBe(" ");
+  });
+
+  it("should test phone number fields", () => {
+    fireEvent.focus(getByTestId("phone-number"));
+    expect(getByTestId("validation-phone-number").innerHTML).toBe(" ");
+    fireEvent.blur(getByTestId("phone-number"));
+    expect(getByTestId("validation-phone-number").innerHTML).toBe(
+      "please enter the name "
+    );
+    fireEvent.change(getByTestId("phone-number"), { target: { value: "12" } });
+    expect(getByTestId("validation-phone-number").innerHTML).toBe(
+      "please enter the 10 digit phone number"
+    );
+    fireEvent.change(getByTestId("phone-number"), {
+      target: { value: "9989984350" }
+    });
+    expect(getByTestId("validation-phone-number").innerHTML).toBe("");
   });
 });
